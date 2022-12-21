74e7831a8dda7c52ba04134797998fae4559ae76
Steve Passarelli <passarellism@gmail.com>
2022-12-21 09:53:07 -0700

    commit tree d3da4bd70e7193b3e0667a0bb7093b3e0b76f7cf

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(arrayOfNames, eventName) {
+    let newArray = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        newArray.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+
+    return newArray;
+}
+
+function countDown(number){
+    while (0 < number) {
+        console.log(number);
+        number--;
+    }
+
+    console.log(number);
+}
+
+// countdown(10);
